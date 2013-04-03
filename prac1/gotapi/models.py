c9f63280af6d94e75bb6202679362fcda82e6b93
Adri <adri@adri-K52F.(none)>
2013-04-03 12:43:57 +0200

    beta3

--- prac1/gotapi/models.py
+++ prac1/gotapi/models.py
@@ -2,24 +2,28 @@ from django.db import models
 
 # Create your models here.
 
-
-class House(models.Model):
-
-	name = models.CharField(max_length=80)
-	def __unicode__(self):
-		return self.name
-
 class Place(models.Model):
 
 	name = models.CharField(max_length=80)
 	def __unicode__(self):
 		return self.name
 
+class Castle(models.Model):
+	name = models.CharField(max_length=80)
+
+class House(models.Model):
+	name = models.CharField(max_length=80)
+	slogan = models.CharField(max_length=80)
+	place = models.ForeignKey(Place)
+	
+	def __unicode__(self):
+		return self.name
+
 class Person(models.Model):
 
-	
-	name = models.CharField(max_length=80)
-	familiars = [] 	
+	name = models.CharField(max_length=80) 	
+	#father = models.ForeignKey(Person, related_name='Mother')
+	#mother = models.ForeignKey(self, related_name='Father')
 	house = models.ForeignKey(House)
 	place = models.ForeignKey(Place)
 	def __unicode__(self):
